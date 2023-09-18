75a6464f92a27c19c4e181377b47dbcbc1fc0a68
Hazel-Heejeong-Nam <hatbi2000@yonsei.ac.kr>
2023-09-18 10:34:07 +0000

    0918

--- train.sh
+++ train.sh
@@ -1,9 +1,32 @@
-c_lr=0.0001
 num_workers=8
 batch_size=512
+seed=1
+sup=weaksup
 
-for l_lr in 0.001 0.0003 0.0001 0.00003 0.00001 0.000003 0.000001; do
-    python main.py --l_lr $l_lr --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
+for c_lr in 0.0001 0.001; do 
+    for c_beta in 4 1; do 
+        python main.py --sup $sup --schedule True --seed $seed --c_beta $c_beta --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
+    done
 done
 
-#0.00003 0.00001 0.000003 0.000001
+
+
+# sup=weaksup
+# python main.py --sup $sup --schedule True --seed $seed --c_beta $c_beta --l_beta $l_beta --l_dag_w1 $l_dag_w1 --l_dag_w2 $l_dag_w2 --l_lr $l_lr --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
+
+
+
+
+# l_dag_w1=3
+# l_dag_w2=0.5
+# python main.py --sup $sup --schedule True --seed $seed --c_beta $c_beta --l_beta $l_beta --l_dag_w1 $l_dag_w1 --l_dag_w2 $l_dag_w2 --l_lr $l_lr --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
+
+
+# l_dag_w1=1.5
+# l_dag_w2=0.25
+# python main.py --sup $sup --schedule True --seed $seed --c_beta $c_beta --l_beta $l_beta --l_dag_w1 $l_dag_w1 --l_dag_w2 $l_dag_w2 --l_lr $l_lr --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
+
+
+# l_dag_w1=0
+# l_dag_w2=0
+# python main.py --sup $sup --schedule True --seed $seed --c_beta $c_beta --l_beta $l_beta --l_dag_w1 $l_dag_w1 --l_dag_w2 $l_dag_w2 --l_lr $l_lr --c_lr $c_lr --num_workers $num_workers --batch_size $batch_size > labelLR_${l_lr}_causalityLR_${c_lr}.txt
